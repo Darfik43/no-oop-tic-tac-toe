b449e16cfe81d2a0dc8303234280c3905d0b0cb7
Darfik43 <Darfik43@gmail.com>
2023-10-05 00:48:58 +0300

    tic-tac-toe v1

---
+++ src/Main.java
@@ -0,0 +1,195 @@
+// no OOP
+// player always makes the move first
+// indexing of cols and rows starts with 0..2
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static int ROW_COUNT = 3;
+    private static int COL_COUNT = 3;
+    private static String CELL_STATE_EMPTY = " " ;
+    private static String CELL_STATE_X = "X" ;
+    private static String CELL_STATE_O = "O" ;
+    private static Scanner scanner = new Scanner(System.in);
+    private static Random random = new Random();
+    private static String GAME_STATE_X_WON = "X won!";
+    private static String GAME_STATE_O_WON = "O won!";
+    private static String GAME_STATE_DRAW = "DRAW!";
+    private static String GAME_STATE_NOT_FINISHED = "Game's not finished!";
+    public static void main(String[] args) {
+
+        while (true) {
+            startGameRound();
+        }
+    }
+
+
+
+    public static String[][] createBoard() {
+        String[][] board = new String[ROW_COUNT][COL_COUNT];
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COL_COUNT; col++) {
+                board[row][col] = CELL_STATE_EMPTY;
+            }
+        }
+        return board;
+    }
+
+    public static void startGameRound() {
+        System.out.println("New round begins");
+        String[][] board = createBoard();
+        startGameLoop(board);
+    }
+
+    public static void startGameLoop(String[][] board) {
+        boolean playerTurn = true;
+
+       do {
+           if (playerTurn) {
+               makePlayerTurn(board);
+               printBoard(board);
+           } else {
+               makeBotTurn(board);
+               printBoard(board);
+           }
+           playerTurn = !playerTurn;
+
+           System.out.println();
+
+
+
+            String gameState = checkGameState(board);
+            if (!Objects.equals(gameState, GAME_STATE_NOT_FINISHED)) {
+                System.out.println(gameState);
+                return;
+            }
+        } while(true);
+    }
+
+    public static void makePlayerTurn(String[][] board) {
+        int[] coordinates = inputCellCoordinates(board);
+
+        board[coordinates[0]][coordinates[1]] = CELL_STATE_X;
+    }
+
+    public static void makeBotTurn(String[][] board) {
+
+        System.out.println("Bot's turn");
+        int[] coordinates = getRandomEmptyCellCoordinates(board);
+        board[coordinates[0]][coordinates[1]] = CELL_STATE_O;
+    }
+
+    public static int[] inputCellCoordinates(String[][] board) {
+        System.out.println("Enter 2 coordinates(row and column) with a space(0-2):");
+        // not checking for spaces and digits
+        do {
+        String[] input = scanner.nextLine().split(" ");
+
+
+            int row = Integer.parseInt(input[0]);
+            int col = Integer.parseInt(input[1]);
+
+            if ((row < 0) || (row >= ROW_COUNT) || (col < 0) || (col >= COL_COUNT)) {
+                System.out.println("Incorrect value! Enter 2 coordinates(row and column) from 0 to 2 with a space:");
+            } else if (!Objects.equals(board[row][col], CELL_STATE_EMPTY)) {
+                System.out.println("This cell is already taken");
+            } else {
+                return new int[]{row, col};
+            }
+        } while (true);
+    }
+
+    public static int[] getRandomEmptyCellCoordinates(String[][] board) {
+        do {
+            int row = random.nextInt(ROW_COUNT);
+            int col = random.nextInt(COL_COUNT);
+
+            if (Objects.equals(board[row][col], CELL_STATE_EMPTY)) {
+                return new int[]{row, col};
+            }
+        } while (true);
+    }
+
+    public static String checkGameState(String[][] board) {
+        ArrayList<Integer> sums = new ArrayList<>();
+
+        for (int row = 0; row < ROW_COUNT; row++) {
+            int rowSum = 0;
+            for (int col = 0; col < COL_COUNT; col++) {
+                rowSum += calculateNumValue(board[row][col]);
+            }
+            sums.add(rowSum);
+        }
+
+        for (int col = 0; col < COL_COUNT; col++) {
+            int colSum =0;
+            for (int row = 0; row < ROW_COUNT; row++) {
+                colSum += calculateNumValue(board[row][col]);
+            }
+            sums.add(colSum);
+        }
+        // from bottom-left to top-right
+        int leftDiagonalSum = 0;
+        for (int i = 0; i < ROW_COUNT; i++) {
+            leftDiagonalSum += calculateNumValue(board[i][i]);
+        }
+        sums.add(leftDiagonalSum);
+
+        // from bottom-right to top-left
+        int rightDiagonalSum = 0;
+        for (int i = 0; i < ROW_COUNT; i++) {
+            rightDiagonalSum += calculateNumValue(board[i][(ROW_COUNT - 1) - i]);
+        }
+        sums.add(leftDiagonalSum);
+
+        if (sums.contains(3)) {
+            return GAME_STATE_X_WON;
+        } else if (sums.contains(-3)) {
+            return GAME_STATE_O_WON;
+        } else if(areAllCellsTaken(board)){
+            return GAME_STATE_DRAW;
+        } else {
+            return GAME_STATE_NOT_FINISHED;
+        }
+    }
+    // X - 1, O - (-1), empty - 0
+    // checking for winner
+    private static int calculateNumValue(String cellState) {
+        if (Objects.equals(cellState, CELL_STATE_X)) {
+            return 1;
+        } else if (Objects.equals(cellState, CELL_STATE_O)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static boolean areAllCellsTaken(String[][] board) {
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COL_COUNT; col++) {
+                if (Objects.equals(board[row][col], CELL_STATE_EMPTY)) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public static void printBoard(String[][] board) {
+        System.out.println("---------");
+        for (int row = 0; row < ROW_COUNT; row++) {
+            String line = "| ";
+            for (int col = 0; col < COL_COUNT; col++) {
+                line += board[row][col] + " ";
+            }
+            line += "|";
+
+            System.out.println(line);
+        }
+        System.out.println("---------");
+    }
+}
